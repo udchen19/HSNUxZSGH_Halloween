fe32425c947b69a85d31be325f5a3074290b4143
udchen19 <72263388+udchen19@users.noreply.github.com>
2020-10-09 21:04:53 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
-
-var loc = window.location.pathname;
-var dir = "http://localhost/NCTU/General/FCS/2020/%E8%90%AC%E8%81%96RPG/%E9%81%93%E5%85%B7/Nonogram/";
-
-fetch(dir + 'puzzle.txt').then(response => response.text()).then((data) => {
-    console.log(response.text);
-});
+window.onload = setup;
 
 function setup() {
-    var fr = new FileReader();
-    var fl = new File(["foo", "foo2"], "puzzle.txt", {
-        type: "text/plain",
-      });
-    fr.addEventListener("loadend", function() {
-        console.log(fr.result);
-     });
-    fr.readAsText(fl);
+    fetch("https://udchen19.github.io/HSNUxZSGH_Halloween/puzzle.txt").then(response => response.text()).then((data) => {
+        console.log(data);
+    });
+    var grid_str = "<table>";
+    for (var i = 0; i < 25; i++) {
+        grid_str += "<tr>";
+        for (var j = 0; j < 25; j++)
+            grid_str += "<td></td>";
+        grid_str += "</tr>";
+    }
+    grid_str += "</table>";
+    document.getElementById("grid").innerHTML = grid_str;
 }
-
-window.onload = setup;
